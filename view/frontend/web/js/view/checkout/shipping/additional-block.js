84b7a29a516ef94728016f2d454fc82935c0946f
Ardit Zavalina <35269845+arditza@users.noreply.github.com>
2023-08-01 21:24:37 +0200

    minor fixes

--- view/frontend/web/js/view/checkout/shipping/additional-block.js
+++ view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -5,28 +5,28 @@ define([
 ], function (Component,quote,ko) {
     'use strict';
 
-    var shipping_options = window.checkoutConfig.shipping_options;
-    var shipping_options_title = window.checkoutConfig.shipping_options_title;
-    var ignored_shipping_methods = window.checkoutConfig.ignored_shipping_methods;
-
     return Component.extend({
         defaults: {
             template: 'Azra_ShippingOptions/checkout/shipping/additional-block',
-            shipping_options: shipping_options,
-            title: shipping_options_title,
-            ignored_shipping_methods: ignored_shipping_methods,
+            shipping_options: window.checkoutConfig.shipping_options,
+            title: window.checkoutConfig.shipping_options_title,
+            ignored_shipping_methods: window.checkoutConfig.ignored_shipping_methods,
             shouldDisplay: ko.observable(!quote.isVirtual())
         },
+
         initialize: function() {
             this._super();
-            quote.shippingMethod.subscribe((function(shippingMethod) {
+
+            quote.shippingMethod.subscribe(function(shippingMethod) {
                 let shippingMethodcode = shippingMethod.carrier_code + "_" + shippingMethod.method_code;
                 let ignoredShippingMethod = this.ignored_shipping_methods.indexOf(shippingMethodcode) !== -1;
+
                 this.shouldDisplay(!ignoredShippingMethod && !quote.isVirtual());
-            }).bind(this));
+            }, this);
         },
+
         getShippingOptions: function(){
-        	return shipping_options;
+        	return this.shipping_options;
         }
     });
 });
